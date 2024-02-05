e0be87f5098e574c21f50b881dc19136c2f0de5d
Matheus Sousa <68212560+matheussousaofc@users.noreply.github.com>
2024-02-05 00:28:57 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+
+const botao = document.querySelector(".btn-plataforma");
+console.log(botao);
+
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.toggle("ativo");
+});
